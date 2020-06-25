4084e75dbafa2d37b80485c696f311d38e6a973e
Oliver Cass <olicass99@gmail.com>
2020-06-25 13:15:24 +0100

    Delete contact.php

--- contact/contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = wordwrap($_POST["message"], 70);
-mail("treasurer.leedsxrsociety@gmail.com", "Contact via Website from $name", $message, "From: $email\r\nCC: faculty.leedsxrsociety@gmail.com");
-?>
